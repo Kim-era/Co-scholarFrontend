9135eed4bc7f3ca53aef70cb96d99b0af9aa7d05
Kim-era <112277553+Kim-era@users.noreply.github.com>
2026-01-29 06:53:21 +0300

    Delete routes directory
    not needed

--- routes/search.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { searchThreads } from '../controllers/searchController.js';
-
-router.get('/', searchThreads);
-
-export default router;
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import userController from "../controllers/userController";
-
-router.post("/", userController.createUser);
-router.get("/", userController.getUsers);
-
-export default router;
