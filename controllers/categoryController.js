8b258345fd6e4c13593b75f4aa73afa6f51c4d85
Kim-era <112277553+Kim-era@users.noreply.github.com>
2026-01-29 06:54:25 +0300

    Delete controllers directory
    not needed

--- controllers/categoryController.js
+++
@@ -1,8 +0,0 @@
-import db from "../config/db.js";; // your MySQL connection
-
-export const getCategories = async (req, res) => {
-  const [rows] = await db.query("SELECT * FROM categories");
-  res.json(rows);
-};
-
-  
